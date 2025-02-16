c264759e776287bb5916d686ab1b749ad7e8a2cb
arunkumar3783 <mr.arun3783@gmail.com>
2025-02-16 22:21:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ import tempfile
 st.title("Human Screen Detection App")
 
 filename="model.pk"
-with open(path.join("model",filename),'rb') as f:
+with open(path.join(filename),'rb') as f:
     model=pickle.load(f)
 
 filename2="label_encoder.pkl"
-with open(path.join("model",filename2),'rb') as l:
+with open(path.join(filename2),'rb') as l:
     label_encoder=pickle.load(l)
     
 # Function to extract features from an audio file
